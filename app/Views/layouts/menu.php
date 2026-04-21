fb9c2cec1edf4a5fc83b8ec904ec050cd0fcc7af
muhamadrobirahmawan70-droid <muhamadrobirahmawan70@gmail.com>
2026-04-21 13:03:43 +0700

    28

---
+++ app/Views/layouts/menu.php
@@ -0,0 +1,24 @@
+   <a href="#">
+        <b>Maldin17</b>App
+    </a>
+    <br>
+Masuk sebagai: <b><?= session('nama'); ?> (<?= session('role'); ?>)</b>
+<br>
+<img src="<?= base_url('uploads/users/' . session()->get('foto')) ?>" height="80" />
+        <br>
+    <br>
+
+    <a href="<?= base_url('/') ?>">
+        Dashboard
+    </a>
+    <br>
+        <?php if (session()->get('role') == 'admin' || session()->get('role') == 'petugas') : ?>
+        <a href="<?= base_url('/users') ?>">
+            Users
+        </a><br>
+    <?php endif; ?>
+    <?php $idu = session('id'); ?>
+    <a href="<?= base_url('users/edit/' . $idu) ?>">
+        Setting
+    </a>
+    <br><a href="<?= site_url('/logout') ?>"> Log Out </a><br>
